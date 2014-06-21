c68598b9247ea1466f9bf84c4f6205e7e484f51c
hicrib <hbuyucek@gmail.com>
2014-06-21 15:01:15 +0300

    assignment done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## given a is an invertible matrix
+## if you make an object holding a matrix like this : matObj <- makeCacheMatrix(a)
+## calling like this cacheSolve(matObj) will return cached inverse of matrix if possible. 
+## if no-cached data, it'll solve() for a that's stored in matObj
+## it doesn't matter if a changes, what's stored in matObj will be used.
 
-## Write a short comment describing this function
-
+## Creates an object that holds a matrix "x" and 4 functions called set,get,setmean,getmean
+## 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) {
+		m <<- mean
+	}
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
+## returns the inverse of a matrix that is encapsulated in matObj. takes advantage of caching if available
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		  m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+		message("uncached-> solving")
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
 }
